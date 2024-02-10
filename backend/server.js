57fcb56e67b1a572885701c3209d6501a497d246
Ravi Gangwar <113000419+ravi-gangwar@users.noreply.github.com>
2024-02-11 01:12:06 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,14 +15,14 @@ const app = express();
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://green-earth-chi.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'https://green-earth-pink.vercel.app/');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 app.use(
   cors({
-    origin: "*",
+    origin: "https://green-earth-pink.vercel.app/",
   })
 );
 
