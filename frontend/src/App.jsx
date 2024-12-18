41b531e09e185fc4faeb1b05550afc11d6f4fded
Ravi Gangwar <ravigangwar7465@gmail.com>
2024-12-19 00:13:22 +0530

    VISIBLE NAVNBAR

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,18 +34,18 @@ import TreeGrowth from './page/TreeGrowth.jsx';
 
 function App() {
   const userState = useSelector(state => state.loginUserReducer);
-  const {currentUser} = userState;
+  const { currentUser } = userState;
   const isAdmin = currentUser?.data?.isAdmin === true;
 
-  
+
   async function getLocation() {
     const userId = currentUser?.data?._id;
     const name = currentUser?.data?.name;
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
-        async(position) => {
+        async (position) => {
           const { latitude, longitude } = position.coords;
-          await axios.post(`${import.meta.env.VITE_BACKED_DOMAIN}/api/auth/user-location`, {userId, name,  latitude, longitude });
+          await axios.post(`${import.meta.env.VITE_BACKED_DOMAIN}/api/auth/user-location`, { userId, name, latitude, longitude });
         },
       );
     } else {
@@ -53,8 +53,8 @@ function App() {
     }
   }
 
-  useEffect(()=> {
-    if(currentUser){
+  useEffect(() => {
+    if (currentUser) {
       getLocation();
     }
 
@@ -66,48 +66,48 @@ function App() {
     <>{
       (
         <BrowserRouter>
-          {currentUser ? <NavBar/> : ""}
+          <NavBar />
           <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/about' element={<About/>}/> 
-            <Route path='/contact' element={<Contact/>}/>
-            <Route path='/bucket' element={<BucketPage/>}/>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/success' element={<PaySuccess/>}/>
-            <Route path='/cancel' element={<PayCancel/>}/>
-            <Route path="*" element={<NotFound/>}/>
-            <Route path='/treeDetails/:treeId' element={<TreeDetails/>}/>
-            { currentUser ? 
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/bucket' element={<BucketPage />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/success' element={<PaySuccess />} />
+            <Route path='/cancel' element={<PayCancel />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path='/treeDetails/:treeId' element={<TreeDetails />} />
+            {currentUser ?
               <>
-                <Route path='/orders' element={<OrdersList/>}/>
-                <Route path='/garden/:userId' element={<Garden/>}/>
-                <Route path='/profile' element={<UserProfile/>}/>
-                <Route path='/get-membership' element={<GetMembership/>}/>
-                <Route path='/plant-tree/:treeId' element={<PlantTree/>}/>
-                <Route path='/tree-growth-track/:treeId' element={<TreeGrowth/>}/>
+                <Route path='/orders' element={<OrdersList />} />
+                <Route path='/garden/:userId' element={<Garden />} />
+                <Route path='/profile' element={<UserProfile />} />
+                <Route path='/get-membership' element={<GetMembership />} />
+                <Route path='/plant-tree/:treeId' element={<PlantTree />} />
+                <Route path='/tree-growth-track/:treeId' element={<TreeGrowth />} />
               </> : ""
             }
 
 
-            {isAdmin || currentUser?.data.isWorker  ? 
-            <>
-            <Route path='/admin/users' element={<UserList/>}/>
-            <Route path='/admin' element={<AdminScreen/>}/>
-            <Route path='/admin/trees' element={<TreeList/>}/>
-            <Route path='/admin/add-tree' element={<AddTree/>}/>
-            <Route path='/admin/orders' element={<AdminOrders/>}/>
-            <Route path='/admin/edit-tree/:treeId' element={<EditTree/>}/>
-            <Route path='/admin/user-messages' element={<UserMessages/>}/>
-            </> : ""}
+            {isAdmin || currentUser?.data.isWorker ?
+              <>
+                <Route path='/admin/users' element={<UserList />} />
+                <Route path='/admin' element={<AdminScreen />} />
+                <Route path='/admin/trees' element={<TreeList />} />
+                <Route path='/admin/add-tree' element={<AddTree />} />
+                <Route path='/admin/orders' element={<AdminOrders />} />
+                <Route path='/admin/edit-tree/:treeId' element={<EditTree />} />
+                <Route path='/admin/user-messages' element={<UserMessages />} />
+              </> : ""}
           </Routes>
-            <Footer/>
+          <Footer />
         </BrowserRouter>
 
       )
 
 
-      }
-  </>
+    }
+    </>
   )
 }
 
